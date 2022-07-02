c43c0d7e641fc9b8240cc6a68f41931b89b09db7
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-02 08:03:55 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int  ainteger;
 long along;
 long long  alonglong;
 float afloat;
-printf("Size of a char: %lubyte(s)\n", sizeof(acharacter));
-printf("Size of an int: %lubyte(s)\n", sizeof(ainteger));
-printf("Size of a long int: %lubyte(s)\n", sizeof(along));
-printf("Size of a long long int: %lubyte(s)\n", sizeof(alonglong));
-printf("Size of a float: %lubyte(s)\n", sizeof(afloat));
+printf("Size of a char: 1 byte(s)\n", sizeof(acharacter));
+printf("Size of an int: 4 ubyte(s)\n", sizeof(ainteger));
+printf("Size of a long int: 8 byte(s)\n", sizeof(along));
+printf("Size of a long long int: 8 byte(s)\n", sizeof(alonglong));
+printf("Size of a float: 4 byte(s)\n", sizeof(afloat));
 return (0);
 }
