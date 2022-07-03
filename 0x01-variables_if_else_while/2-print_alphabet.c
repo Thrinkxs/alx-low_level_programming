0c47b2710fb8ad5123b2e65f83efb34802112425
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-03 03:46:29 -0700

    comparing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get random number and compare it 5
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+
+putchar('\n');
+return (0);
+}
