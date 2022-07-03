f1581eb2b79515e5b6ee0e5773b63f8462f4a95c
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-03 05:30:43 -0700

    combo1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * man -> assign a random number to the variable each time it is executed
+ * prints last digit of th nuumber stored in the variable n
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+int n;
+for (ch = 48; ch <= 57; ch++)
+{
+for (n = 49; n <= 57; n++)
+{
+if (n > ch)
+{
+putchar(ch);
+putchar(n);
+if (ch != 56 || n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+putchar(10); /**
+this is an ascii code for the new line
+	       */
+return (0);
+}
