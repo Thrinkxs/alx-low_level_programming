ffbaa6a0d07fc3d062bda6f949b78ba66100846e
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-08 17:55:57 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,14 +12,13 @@ end_num = 1024;
 total = 0;
 for (start_num = 0; start_num < end_num; start_num++)
 {
-if ((start_num % 3 == 0 || (start_num % 5 == 0))
+if ((start_num % 3 == 0) || (start_num % 5 == 0))
 {
 total = total + start_num;
 }
 else
 {
 continue;
-}
 }
 printf("%d", total);
 printf("\n");
