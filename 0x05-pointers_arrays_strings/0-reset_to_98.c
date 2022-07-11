9b55d7bd9a3b3854cf19d1f9a1c43ef9ba247511
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-11 11:10:44 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - function that takes a pointer to an integer parameterr
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
