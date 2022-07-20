bbb28bc7fee0435b5aa420fa9dfb0cab31ddb67c
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-20 04:45:12 -0700

    corrected 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y.
  * @x: base.
- * 2y: exponent.
+ * @y: exponent.
  * return: value of the exponentiation.
  */
 int _pow_recursion(int x, int y)
