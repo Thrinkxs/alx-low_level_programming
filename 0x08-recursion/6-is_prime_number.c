8f594bb2813272007eb357d966467c10eb4430ec
Emmanuel Omokhegbe <emmanuelozigue@gmail.com>
2022-07-20 05:15:56 -0700

    corrected 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * is_prme - detects if an input number is a prime number.
+ * is_prime - detects if an input number is a prime number.
  * @n: input number.
  * @c: iterator.
  * Return: 1 if n is a prime number, 0 if n is not a prime number.
@@ -17,7 +17,7 @@ return (0);
 return (0 + is_prime(n, c + 1));
 }
 /**
- * is _prime_number - detects if an input number is a prime number.
+ * is_prime_number - detects if an input number is a prime number.
  * @n: input number.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
