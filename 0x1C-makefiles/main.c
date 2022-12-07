2f9b7a3da92d5744d159c642bbcc5d18655936a4
Emmanuel <emmanuelozigue@gmail.com>
2022-12-07 10:28:14 -0800

    makefiles

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,25 @@
+#include "m.h"
+
+
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always 
+
+ */
+
+int main(void)
+  
+{
+  
+  print_holberton();
+  
+  return (EXIT_SUCCESS);
+  
+}
+
+/* School */
